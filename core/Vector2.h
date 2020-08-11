631abafa6e19724ecebf545d376c1c28b44579ed
ArcziPT <arek.w999@gmail.com>
2020-08-11 19:53:15 +0200

    Vector2 added

---
+++ core/Vector2.h
@@ -0,0 +1,55 @@
+#ifndef VECTOR2_H
+#define VECTOR2_H
+
+#include <memory>
+
+class Vector2{
+
+public:
+    //const
+    Vector2() = default;
+
+	Vector2(double x, double y) : x(x), y(y) {}
+	Vector2(const Vector2& vec): x(vec.x), y(vec.y) {}
+    Vector2(const std::unique_ptr<Vector2>& vec): x(vec->x), y(vec->y) {}
+    Vector2(double r, double theta);
+
+    //special
+    static std::unique_ptr<Vector2> ZERO;
+
+    //TODO: Assigments and copy/move constructors
+
+    //Math
+    Vector2& operator+(const Vector2& vec);
+    Vector2& operator-(const Vector2& vec);
+    double operator*(const Vector2& vec) const;
+    bool operator==(const Vector2& vec) const;
+
+    Vector2& set(double x, double y);
+    Vector2& zero();
+    Vector2& normalize();
+    Vector2& flipVertical(double x);
+    Vector2& flipHorizontal(double y);
+    double len() const;
+    double dist(const Vector2& vec) const;
+
+    double getTheta() const;
+    double getR() const;
+
+    //Quarter
+    enum class Quadrant{
+        FIRST,
+        SECOND,
+        THIRD,
+        FOURTH
+    };
+
+    Quadrant getQuadrant() const;
+
+
+private:
+	 double x;
+	 double y;
+};
+
+#endif
