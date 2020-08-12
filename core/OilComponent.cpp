5961d71684ba4f0ca25e6e516c2a8e5a3e651188
ArcziPT <arek.w999@gmail.com>
2020-08-12 19:25:56 +0200

    OilComponent added

---
+++ core/OilComponent.cpp
@@ -0,0 +1,30 @@
+#include "OilComponent.h"
+
+double OilComponent::getX(){
+    return x;
+}
+
+
+void OilComponent::setX(double x){
+    this->x = x;
+}
+
+
+double OilComponent::getMolecularWeight(){
+    return molecularWeigth;
+}
+
+
+void OilComponent::setMolecularWeight(double molecularWeight){
+    this->molecularWeigth = molecularWeigth;
+}
+
+
+double OilComponent::getTb(){
+    return tb;
+}
+
+
+void OilComponent::setTb(double tb){
+    this->tb = tb;
+}
---
+++ core/OilComponent.h
@@ -0,0 +1,27 @@
+#ifndef OIL_COMPONENT_H
+#define OIL_COMPONENT_H
+
+class OilComponent{	
+
+public:
+
+	OilComponent(double x, double molecularWeigth, double tb): x(x), molecularWeigth(molecularWeigth), tb(tb) {}
+
+    double getX();
+    void setX(double x);
+    
+    double getMolecularWeight();
+    void setMolecularWeight(double molecularWeight);
+
+    double getTb();
+    void setTb(double tb);
+
+private:
+
+    double x; // ulamek masowy
+	double molecularWeigth; // masa molowa kg/moll
+	double tb; // temperatura wrzenia w K
+
+};
+
+#endif
